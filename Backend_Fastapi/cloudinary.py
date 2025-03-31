4711f1ad16d0a63d758d2562590aac47d2420f0f
BiMalxMe <journeyxbimal@gmail.com>
2025-03-31 12:15:41 +0545

    AddintheImageToTheCloudinayServer

--- Backend_Fastapi/cloudinary.py
+++ Backend_Fastapi/cloudinary.py
@@ -14,3 +14,15 @@ cloudinary.config(
     api_key=os.getenv('CLOUDINARY_API_KEY'),
     api_secret=os.getenv('CLOUDINARY_API_SECRET')
 )
+
+#Doing on the basis of Cloudinary SDK
+def upload_image_to_cloudinary(image_data: bytes):
+    try:
+        # Upload the image to Cloudinary
+        response = cloudinary.uploader.upload(
+            image_data,
+            resource_type="auto",  # Automatically detects the file type(Image type)
+        )
+        return response
+    except Exception as e:
+        return {"error": str(e)}
