e1e02d42a5bd667bf9efabe816d3eb82868c8f34
BiMalxMe <journeyxbimal@gmail.com>
2025-03-31 08:23:26 +0545

    AddingTheModelApikeyAndApiUrlWithInputValidation

--- Backend_Fastapi/main.py
+++ Backend_Fastapi/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import os
+import requests
+from dotenv import load_dotenv
+
+
+#IntitalizingTheFastApiAPp
+app = FastAPI()
 
 
 # We should import the package pip install dot-env to access the variables of the main .env file
@@ -7,11 +14,21 @@ import os
 # secret_key=os.getenv("Secret") ,Insidee the function there we can put the vatiable available in the .env
 
 
-from dotenv import load_dotenv
+# This does the validation of data imported from the pydantic library
+#  If it is expecting text as a string then if int is passed it throws error
+class HuggingFace(BaseModel):
+    text: str
 
+#In this we are taking the api key by using .env variable
+secret_key=os.getenv("Huggingface_Token")
 
-secret_key=os.getenv("Secret")
-app = FastAPI()
+#The model name we are using is for the text to image found in the Hugging face
+# ITs ai model and our data is sent and processed at thee models environmet
+MODEL_NAME = "CompVis/stable-diffusion-v-1-4-original"
+
+#This is the main urL of the Hugging face where our input adata are sent to
+HUGGING_FACE_URL = f"https://api-inference.huggingface.co/models/{MODEL_NAME}"
+
 
 @app.get("/")
 def read_root():
