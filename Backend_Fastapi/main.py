b12cd59d3e8f3e45525ccf28c1fd9dcfb5ca03b6
BiMalxMe <journeyxbimal@gmail.com>
2025-03-31 07:40:53 +0545

    AddingSomeInfoAboutTheAcessOfEnvVaiables

--- Backend_Fastapi/main.py
+++ Backend_Fastapi/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import os
 
+
+# We should import the package pip install dot-env to access the variables of the main .env file
+# As we can acess and To get the specific vaiables we need to do
+# secret_key=os.getenv("Secret") ,Insidee the function there we can put the vatiable available in the .env
+
+
+from dotenv import load_dotenv
+
+
+secret_key=os.getenv("Secret")
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World","Secret":secret_key}
