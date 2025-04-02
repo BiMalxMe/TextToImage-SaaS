ce539e1e7ce371235109a483e6258b3153f066f2
BiMalxMe <journeyxbimal@gmail.com>
2025-04-02 17:08:20 +0545

    RemovingtheClerkAuthentication

--- nextjs-frontend/app/page.tsx
+++ nextjs-frontend/app/page.tsx
@@ -1,13 +1,7 @@
-// app/page.tsx
-
-import { SignIn } from "@clerk/nextjs";
-
-export default function Home() {
-  
+export default function Home(){
   return (
     <div>
-      <h1>Welcome to My App</h1>
-      Bimal chalise swagating you
+      bimal chalise
     </div>
-  );
+  )
 }
