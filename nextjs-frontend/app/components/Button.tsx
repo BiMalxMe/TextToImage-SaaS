cab50ed25ee6b07d25196e263b72438d27922894
BiMalxMe <journeyxbimal@gmail.com>
2025-04-04 07:21:04 +0545

    AddingtheCustomBtnWithTextParameter

---
+++ nextjs-frontend/app/components/Button.tsx
@@ -0,0 +1,16 @@
+type DarkButtonProps = {
+    text: string;
+    onClick?: () => void;
+  };
+  
+  export default function DarkButton({ text, onClick }: DarkButtonProps) {
+    return (
+      <button
+        onClick={onClick}
+        className="bg-gray-800 text-white px-6 py-2 rounded-md hover:bg-gray-700 transition-colors duration-300 shadow-md focus:outline-none focus:ring-2 focus:ring-gray-500"
+      >
+        {text}
+      </button>
+    );
+  }
+  
