59cc98d1236d856a088dcad5d08b8d0c6fb00fc0
BiMalxMe <journeyxbimal@gmail.com>
2025-04-05 08:57:14 +0545

    LandingPage

--- nextjs-frontend/app/checkout/page.tsx
+++ nextjs-frontend/app/checkout/page.tsx
@@ -5,14 +5,13 @@ export default function PaymentPage() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#0f0f0f] text-white p-4">
       {/* Container with gradient */}
       <div className="bg-gradient-to-br from-purple-600 via-pink-500 to-red-400 p-6 rounded-2xl shadow-lg max-w-md w-full mb-6 ">
-        <h2 className="text-xl font-semibold mb-4 text-center text-white">FOR PAYPAL PAYMENT</h2>
+        <h2 className="text-xl font-semibold mb-4 text-center text-white">FOR KHALTI PAYMENT</h2>
        <div  className="flex justify-center"> <KhaltiButton amount={10000} /></div>
       </div>
 
       {/* Another container with gradient */}
-      <div className="bg-gradient-to-br from-indigo-600 via-blue-500 to-cyan-400 p-6 rounded-2xl shadow-lg max-w-md w-full">
-        <h2 className="text-xl font-semibold mb-4 text-center text-white">FOR KHALTI PAYMENT</h2>
-        <div  className="flex justify-center"> <KhaltiButton amount={10000} /></div>
+      <div className="tooltip tooltip-bottom bg-gradient-to-br from-indigo-600 via-blue-500 to-cyan-400 p-6 rounded-2xl shadow-lg max-w-md w-full"  data-tip="Coming Very Soon.......">
+        <h2 className="text-xl font-semibold mb-4 text-center text-white">FOR PAYPAL PAYMENT</h2>
       </div>
     </div>
   );
