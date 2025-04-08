a57b2ef77b20264bd81c17380ea27a66bad88058
BiMalxMe <journeyxbimal@gmail.com>
2025-04-08 06:06:53 +0545

    InCardFixReloadingOfImageInConfigFixedTheImageSrcFromCloudFromShareFixTheStrokeCaps

--- nextjs-frontend/next.config.ts
+++ nextjs-frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/dvb8lhl7t/image/upload/**',
+      },
+    ],
+  },
   /* config options here */
 };
+
 
 export default nextConfig;
